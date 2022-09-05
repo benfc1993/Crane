189261e325787868c72375b884fed0c7a8fa6baa
Ben <benfeldbergcollins@hotmail.co.uk>
2022-09-05 22:32:31 +0100

    add Buffer Layout

--- Crane/src/Crane/Core/Application.cpp
+++ Crane/src/Crane/Core/Application.cpp
@@ -1,17 +1,37 @@
-#include "crpch.h"
-
 #include "Crane/Core/Application.h"
-#include "Crane/Core/Log.h"
-#include "Crane/Input/Input.h"
 
 #include <glad/glad.h>
+
+#include "Crane/Core/Log.h"
+#include "Crane/Input/Input.h"
+#include "crpch.h"
 
 namespace Crane
 {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
-    Application *Application::s_Instance = nullptr;
+    Application* Application::s_Instance = nullptr;
+
+    static GLenum ShaderDataTypeToOpenGLDataType(ShaderDataType type) {
+        switch (type)
+        {
+        case Crane::ShaderDataType::Float:      return GL_FLOAT;
+        case Crane::ShaderDataType::Float2:     return GL_FLOAT;
+        case Crane::ShaderDataType::Float3:     return GL_FLOAT;
+        case Crane::ShaderDataType::Float4:     return GL_FLOAT;
+        case Crane::ShaderDataType::Mat3:       return GL_FLOAT;
+        case Crane::ShaderDataType::Mat4:       return GL_FLOAT;
+        case Crane::ShaderDataType::Int:        return GL_INT;
+        case Crane::ShaderDataType::Int2:       return GL_INT;
+        case Crane::ShaderDataType::Int3:       return GL_INT;
+        case Crane::ShaderDataType::Int4:       return GL_INT;
+        case Crane::ShaderDataType::Bool:       return GL_BOOL;
+        }
+
+        CR_CORE_ASSERT(false, "Invalid shader data type!");
+        return 0;
+    }
 
     Application::Application()
     {
@@ -27,29 +47,47 @@ namespace Crane
         glGenVertexArrays(1, &m_VertexArray);
         glBindVertexArray(m_VertexArray);
 
-        float vertices[3 * 3] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f, 0.5f, 0.0f};
+        float vertices[3 * 7] = {
+            -0.5f, -0.5f, 0.0f, 0.8f, 0.13f, 0.3f, 1.0f,
+            0.5f, -0.5f, 0.0f, 0.3f, 0.8f, 0.13f, 1.0f,
+            0.0f, 0.5f, 0.0f, 0.13f, 0.3f, 0.8f, 1.0f,
+        };
 
         m_VertexBuffer.reset(VertexBuffer::Create(sizeof(vertices), vertices));
 
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        BufferLayout layout = {
+            {ShaderDataType::Float3, "a_Position"},
+            {ShaderDataType::Float4, "a_Color"}
+        };
 
-        uint32_t indices[3] = {0, 1, 2};
+        m_VertexBuffer->SetLayout(layout);
+
+        uint32_t index = 0;
+        for (const auto& element : m_VertexBuffer->GetLayout())
+        {
+            glEnableVertexAttribArray(index);
+            glVertexAttribPointer(index, element.GetComponentCount(), ShaderDataTypeToOpenGLDataType(element.Type),
+                element.Normalized ? GL_TRUE : GL_FALSE, layout.GetStride(), (const void*)element.Offset);
+            index++;
+        }
+
+
+        uint32_t indices[3] = { 0, 1, 2 };
         m_IndexBuffer.reset(IndexBuffer::Create(sizeof(indices) / sizeof(u_int32_t), indices));
 
         std::string vertexSrc = R"(
             #version 330 core
 
             layout(location = 0) in vec3 a_Position;
+            layout(location = 1) in vec4 a_Color;
 
             out vec3 v_Position;
+            out vec4 v_Color;
 
             void main()
             {
                 v_Position = a_Position;
+                v_Color = a_Color;
                 gl_Position = vec4(a_Position, 1.0);
             }
         )";
@@ -60,10 +98,12 @@ namespace Crane
             layout(location = 0) out vec4 color;
             
             in vec3 v_Position;
+            in vec4 v_Color;
 
             void main()
             {
                 color = vec4(v_Position * 0.5 + 0.5 , 1.0);
+                color = v_Color;
             }
         )";
 
@@ -74,19 +114,19 @@ namespace Crane
     {
     }
 
-    void Application::PushLayer(Layer *layer)
+    void Application::PushLayer(Layer* layer)
     {
         m_LayerStack.PushLayer(layer);
         layer->OnAttach();
     }
 
-    void Application::PushOverlay(Layer *overlay)
+    void Application::PushOverlay(Layer* overlay)
     {
         m_LayerStack.PushLayer(overlay);
         overlay->OnAttach();
     }
 
-    void Application::OnEvent(Event &e)
+    void Application::OnEvent(Event& e)
     {
         EventDispatcher dispatcher(e);
         dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
@@ -111,11 +151,11 @@ namespace Crane
             glBindVertexArray(m_VertexArray);
             glDrawElements(GL_TRIANGLES, m_IndexBuffer->GetCount(), GL_UNSIGNED_INT, nullptr);
 
-            for (Layer *layer : m_LayerStack)
+            for (Layer* layer : m_LayerStack)
                 layer->OnUpdate();
 
             m_ImGuiLayer->Begin();
-            for (Layer *layer : m_LayerStack)
+            for (Layer* layer : m_LayerStack)
                 layer->OnImGuiRender();
             m_ImGuiLayer->End();
 
@@ -123,9 +163,9 @@ namespace Crane
         }
     }
 
-    bool Application::OnWindowClose(WindowCloseEvent &e)
+    bool Application::OnWindowClose(WindowCloseEvent& e)
     {
         m_Running = false;
         return true;
     }
-}
+} // namespace Crane
--- Crane/src/Platform/OpenGL/OpenGLBuffer.h
+++ Crane/src/Platform/OpenGL/OpenGLBuffer.h
@@ -7,24 +7,28 @@ namespace Crane
     class OpenGLVertexBuffer : public VertexBuffer
     {
     public:
-        OpenGLVertexBuffer(uint32_t size, float *vertices);
+        OpenGLVertexBuffer(uint32_t size, float* vertices);
         virtual ~OpenGLVertexBuffer();
 
-        virtual void Bind() const;
-        virtual void Unbind() const;
+        virtual void Bind() const override;
+        virtual void Unbind() const override;
+
+        virtual const BufferLayout& GetLayout() const override { return m_Layout; };
+        virtual void SetLayout(BufferLayout& layout) override { m_Layout = layout; };
 
     private:
         uint32_t m_RendererId;
+        BufferLayout m_Layout;
     };
 
     class OpenGLIndexBuffer : public IndexBuffer
     {
     public:
-        OpenGLIndexBuffer(uint32_t count, uint32_t *indices);
+        OpenGLIndexBuffer(uint32_t count, uint32_t* indices);
         virtual ~OpenGLIndexBuffer();
 
-        virtual void Bind() const;
-        virtual void Unbind() const;
+        virtual void Bind() const override;
+        virtual void Unbind() const override;
 
         virtual uint32_t GetCount() const { return m_Count; }
 
