85f759f229f9198a1edc16ab2f3c3129d5f1bbe9
benfc1993 <benfeldbergcollins@hotmail.co.uk>
2022-10-05 22:39:36 +0100

    Revert "add windows setup file"
    This reverts commit f8538e3d3000d5f0ff1328d8ecc977663874cc18.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 cd ../
-vendor/bin/linux/premake5 gmake2
-vendor/bin/linux/premake5 vscode
+vendor/bin/premake5 gmake2
+vendor/bin/premake5 vscode
 rm -rf Crane/vendor/GLAD/.vscode
 rm -rf Crane/vendor/GLFW/.vscode
 rm -rf Crane/vendor/imgui/.vscode
