738727e22d1ab3b2fb82d1a7c94f5775da962b20
Ben <benfeldbergcollins@hotmail.co.uk>
2022-10-29 22:34:01 +0100

    fix spdlog

--- Crane/src/Crane/Core/Log.h
+++ Crane/src/Crane/Core/Log.h
@@ -7,7 +7,7 @@
 
 #pragma warning(push, 0)
 #include "spdlog/spdlog.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
+// #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/fmt/ostr.h"
 #pragma warning(pop)
 
